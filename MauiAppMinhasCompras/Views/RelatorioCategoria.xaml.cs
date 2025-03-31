f05772a34ef2994c82c723993b32988be10994b2
Vitoria Régia <Vi.regia216@gmail.com>
2025-03-31 12:22:03 -0300

    Adicionando código para o Relatorio

--- MauiAppMinhasCompras/Views/RelatorioCategoria.xaml.cs
+++ MauiAppMinhasCompras/Views/RelatorioCategoria.xaml.cs
@@ -1,6 +1,7 @@
 namespace MauiAppMinhasCompras.Views;
 using MauiAppMinhasCompras.Models;
 using MauiAppMinhasCompras.Helpers;
+using static MauiAppMinhasCompras.Helpers.SQLiteDatabaseHelper;
 
 public partial class RelatorioCategoria : ContentPage
 {
@@ -10,4 +11,12 @@ public partial class RelatorioCategoria : ContentPage
 		InitializeComponent();
 	}
 
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        List<GastoPorCategoria> dados = await App.Db.GetGastosPorCategoria();
+
+        lst_relatorio.ItemsSource = dados;
+    }
 }
